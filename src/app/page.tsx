78ab0e86e675405d97e023c6b6cc39ffbf1f228f
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-05-12 23:21:34 +0200

    create a `Function for animating an effect of "fade in & slide up".` within `src/app/globals.css`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ const HomePage = () => {
         Fast and simple support ticket management system.
       </p>
 
-      <div className="flex flex-col md:flex-row gap-4 justify-center">
+      <div className="flex flex-col md:flex-row gap-4 justify-center animate-slide opacity-0">
         <Link
           href="/tickets/new"
           className="bg-blue-600 text-white px-6 py-3 rounded shadow hover:bg-blue-700 transition"
