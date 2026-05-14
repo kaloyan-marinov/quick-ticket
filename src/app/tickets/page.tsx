f4f46673d96537319db3b814fb4c99d178f5b089
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-05-14 09:39:57 +0200

    make `<TicketsPage>` render a `<div>` for each fetched ticket

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,10 +1,48 @@
 import { getTickets } from "@/actions/ticket.actions";
+import { logEvent } from "@/utils/sentry";
+import Link from "next/link";
 
 const TicketsPage = async () => {
   const tickets = await getTickets();
-  console.log("tickets", tickets);
+  //   console.log("tickets", tickets);
 
-  return <>tickets</>;
+  return (
+    <div className="min-h-screen bg-blue-50 p-8">
+      <h1 className="text-3xl font-bold text-blue-600 mb-8 text-center">
+        Support Tickets
+      </h1>
+      {tickets.length === 0 ? (
+        <p className="text-center text-gray-600">No Tickets Yet</p>
+      ) : (
+        <div className="space-y-4 max-w-3xl mx-auto">
+          {tickets.map((ticket) => (
+            <div
+              key={ticket.id}
+              className="flex justify-between items-center bg-white rounded-lg shadow border border-gray-200 p-6"
+            >
+              {/* Left Side */}
+              <div>
+                <h2 className="text-xl font-semibold text-blue-600">
+                  {ticket.subject}
+                </h2>
+              </div>
+              <div className="text right spacy-y-2">
+                <div className="text-sm text-gray-500">
+                  Priority: <span>{ticket.priority}</span>
+                </div>
+                <Link
+                  href={`/tickets/${ticket.id}`}
+                  className="inline-block mt-2 bg-blue-600 text-white text-sm px-3 px-1 rounded hover:bg-blue-700 transition text-center"
+                >
+                  View Ticket
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default TicketsPage;
