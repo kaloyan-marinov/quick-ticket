bec196c0434fc39ab11b77f6b8306ce70496275c
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-05-14 12:21:36 +0200

    implement `verifyAuthToken`

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,5 @@
-import { SignJWT } from "jose";
+import { SignJWT, jwtVerify } from "jose";
+// might actually NOT need the `cookies` package
 import { logEvent } from "@/utils/sentry";
 
 const secret = new TextEncoder().encode(process.env.AUTH_SECRET);
@@ -6,6 +7,7 @@ const secret = new TextEncoder().encode(process.env.AUTH_SECRET);
 // the cookie will be called:
 const cookieName = "auth-token";
 
+// Generate, encrypt, and sign token with secret
 export async function signAuthToken(payload: any) {
   try {
     const token = await new SignJWT(payload)
@@ -25,6 +27,33 @@ export async function signAuthToken(payload: any) {
       "auth",
       {
         payload,
+      },
+      "error",
+      error,
+    );
+
+    throw new Error(msg);
+  }
+}
+
+// Decrypt and verify token
+/*
+The `<T>` is a generic type,
+which enables client code to specify the type of the payload
+when said code calls the function.
+*/
+export async function verifyAuthToken<T>(token: string): Promise<T> {
+  try {
+    const { payload } = await jwtVerify(token, secret);
+
+    return payload as T;
+  } catch (error) {
+    const msg = "Token decryption failed";
+    logEvent(
+      msg,
+      "auth",
+      {
+        tokenSnippet: token.slice(0, 10),
       },
       "error",
       error,
