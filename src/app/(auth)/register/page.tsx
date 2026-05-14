73fe6c8fff00ef2afa386bb4055476de085b5e30
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-05-14 13:02:05 +0200

    create a simple `src/app/(auth)/register/page.tsx` (which is static for now)

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,18 @@
+/*
+The path to this file contains `(auth)`
+
+That is just a group (for organization purposes),
+meaning that it's not actually going be part of the URL.
+The URL will just be `/register`.
+*/
+
+const RegisterPage = () => {
+  return (
+    <div className="flex justify-center space-x-4">
+      <h2>«West Side»</h2>
+      <h2>«East Side»</h2>
+    </div>
+  );
+};
+
+export default RegisterPage;
