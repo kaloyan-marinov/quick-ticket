18ff831ca053ca2cf272b7efb5c7815854bc27fa
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-05-14 10:01:19 +0200

    implement a `getTicketById` «server action» and utilize it within the `<TicketDetailsPage>`

--- src/app/tickets/[id]/page.tsx
+++ src/app/tickets/[id]/page.tsx
@@ -1,9 +1,27 @@
+import { getTicketById } from "@/actions/ticket.actions";
+import { notFound } from "next/navigation";
+
 const TicketDetailsPage = async (props: {
   params: Promise<{ id: string }>;
 }) => {
   const { id } = await props.params;
 
-  return <>details {id}</>;
+  const ticket = await getTicketById(id);
+
+  if (!ticket) {
+    notFound();
+  }
+
+  return (
+    <>
+      details:
+      <ul>
+        <li>{ticket.id}</li>
+        <li>{ticket.subject}</li>
+        <li>{ticket.description}</li>
+      </ul>
+    </>
+  );
 };
 
 export default TicketDetailsPage;
