34313e205f117de9b1edcab4dd2d7270b66e27ac
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-05-14 15:58:49 +0200

    get `<RegisterPage>` to be server-rendered, but get the `<RegisterForm>` to be client-rendered (the files at `src/app/(auth)/register/form.tsx` and `src/app/(auth)/login/form.tsx` are so similar that it is worthwhile/instructive to compare them to each other (via your preferred «diff tool»))

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,11 @@
+/*
+The path to this file contains `(auth)`
+
+That is just a group (for organization purposes),
+meaning that it's not actually going be part of the URL.
+The URL will just be `/register`.
+*/
+
 import LoginForm from "./form";
 
 const LoginPage = () => {
