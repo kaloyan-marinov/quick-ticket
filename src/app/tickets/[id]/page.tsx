528761d70bc1df34a4f51edc4f30b6d7aeb86d83
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-05-14 09:52:26 +0200

    enable `<TicketDetailsPage>` to get the ID from the `/tickets/[id]` URL

--- src/app/tickets/[id]/page.tsx
+++ src/app/tickets/[id]/page.tsx
@@ -1,5 +1,9 @@
-const TicketDetailsPage = () => {
-  return <>details</>;
+const TicketDetailsPage = async (props: {
+  params: Promise<{ id: string }>;
+}) => {
+  const { id } = await props.params;
+
+  return <>details {id}</>;
 };
 
 export default TicketDetailsPage;
